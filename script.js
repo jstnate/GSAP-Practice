766ac8ec507408e88f1ea113d8c57bb71b711d7c
Armand Nobre <nobrearmand@gmail.com>
2023-04-04 16:09:58 +0200

    :hammer: mean guy follow

--- script.js
+++ script.js
@@ -47,3 +47,55 @@ function tournerImage(event) {
   TweenMax.to(ship, 1, {rotation:angle, ease:Linear.easeNone, transformOrigin:"50% 50%"})
 }
 window.addEventListener('mousemove', tournerImage);
+
+
+
+const image = document.querySelector('.ship2');
+
+// Fonction pour suivre le mouvement de la souris
+function suivreCurseur(event) {
+  // Position de l'image
+  const imageBounds = image.getBoundingClientRect();
+  const imageCentre = {
+    x: imageBounds.left + imageBounds.width / 2,
+    y: imageBounds.top + imageBounds.height / 2,
+  };
+
+  // Position du curseur
+  const curseur = {
+    x: event.clientX,
+    y: event.clientY,
+  };
+
+  // Calcul de la distance entre l'image et le curseur
+  const distance = Math.hypot(imageCentre.x - curseur.x, imageCentre.y - curseur.y);
+
+  // Calcul de l'angle de rotation pour pointer vers le curseur
+  const angleRad = Math.atan2(curseur.y - imageCentre.y, curseur.x - imageCentre.x);
+  const angleDeg = angleRad * 180 / Math.PI;
+
+  // Utilisation de GSAP pour faire tourner l'image vers le curseur
+  gsap.to(image, {
+    duration: 1.2,
+    ease: 'power2.out',
+    rotation: angleDeg+90,
+    x: curseur.x-110,
+    y: curseur.y-220,
+  });
+
+  // Arrêter l'écouteur d'événements lorsque l'image atteint le curseur
+  if (distance < 50) {
+    window.removeEventListener('mousemove', suivreCurseur);
+    console.log('stop')
+    ship.firstChild.src = "images/nuke_4.jpg"
+    image.style.display = 'none';
+    
+    setTimeout(function(){
+      ship.style.display = 'none';
+    }, 1000);
+
+  }
+}
+
+// Ajout d'un écouteur d'événements pour suivre le mouvement de la souris
+window.addEventListener('mousemove', suivreCurseur);
